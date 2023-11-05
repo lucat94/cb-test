a272833c012ea135ba4c26633bfeb2abf5d15de5
lucatedesco <l.tedesco@brandsdistribution.com>
2023-11-05 21:22:59 +0100

    feat: added few more tests

--- src/player.controller.spec.ts
+++ src/player.controller.spec.ts
@@ -43,23 +43,69 @@ describe("PlayerController", () => {
       expect(players).toHaveProperty("docs");
       expect(players.docs.length).toEqual(10);
     });
+    it("should filter list", async () => {
+      const playersController = app.get(PlayersController);
+      const nameFiler = "Luca";
+      const players = await playersController.getPlayers(
+        nameFiler,
+        10,
+        1,
+        "Player Name",
+        "ASC"
+      );
+      players.docs.map((player) =>
+        expect(player["Player Name"]).toMatch(new RegExp(nameFiler, "i"))
+      );
+    });
+    it("should sort correctly by player name asc", async () => {
+      const playersController = app.get(PlayersController);
+      const playerSortPlayerNameASC = (
+        await playersController.getPlayers("", 1, 1, "Player Name", "ASC")
+      ).docs[0];
+      expect(playerSortPlayerNameASC["Player Name"]).toEqual("Abdou Harroui");
+    });
+    it("should sort correctly by player name desc", async () => {
+      const playersController = app.get(PlayersController);
+      const playerSortPlayerNameDESC = (
+        await playersController.getPlayers("", 1, 1, "Player Name", "DESc")
+      ).docs[0];
+      expect(playerSortPlayerNameDESC["Player Name"]).toEqual(
+        "Łukasz Skorupski"
+      );
+    });
+    it("should sort correctly by yearly salary asc", async () => {
+      const playersController = app.get(PlayersController);
+      const playerSortYearlySalaryASC = (
+        await playersController.getPlayers("", 1, 1, "Yearly Salary", "ASC")
+      ).docs[0];
+      expect(playerSortYearlySalaryASC["Player Name"]).toEqual("Enzo Keutgen");
+    });
+    it("should sort correctly by yearly salary desc", async () => {
+      const playersController = app.get(PlayersController);
+      const playerSortYearlySalarayDESC = (
+        await playersController.getPlayers("", 1, 1, "Yearly Salary", "DESC")
+      ).docs[0];
+      expect(playerSortYearlySalarayDESC["Player Name"]).toEqual(
+        "Dušan Vlahović"
+      );
+    });
   });
-  it("should have salary history", async () => {
-    const playersController = app.get(PlayersController);
-    const players = await playersController.getPlayers(
-      "",
-      10,
-      1,
-      "Yearly Salaray",
-      "DESC"
-    );
-    // expect(result).toHaveProperty("docs");
-    // expect(result.docs.length).toEqual(10);
-    const player = players.docs[0];
+  describe("getPlayerSalaryHistory", () => {
+    it("should have salary history", async () => {
+      const playersController = app.get(PlayersController);
+      const players = await playersController.getPlayers(
+        "",
+        1,
+        1,
+        "Yearly Salaray",
+        "DESC"
+      );
+      const player = players.docs[0];
 
-    const salaryHistory = await playersController.getPlayerSalaryHistory(
-      player._id
-    );
-    expect(salaryHistory.length).toBeGreaterThan(0);
+      const salaryHistory = await playersController.getPlayerSalaryHistory(
+        player._id
+      );
+      expect(salaryHistory.length).toBeGreaterThan(0);
+    });
   });
 });
